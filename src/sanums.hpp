e18e84225c2dc56746c642aa79a2a644cf0183d8
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2022-03-05 14:46:32 +0000

    chore: adding more tests and cleanup

--- src/sanums.hpp
+++ src/sanums.hpp
@@ -96,41 +96,52 @@ namespace detail {
 
 template<concepts::numeric T>
 class basic_strict_num {
+    template<concepts::numeric>
+    friend class basic_strict_num;
 public:
     using value_type = T;
 
     template<concepts::specialisation_of<basic_strict_num> Other>
-    requires(concepts::conversion_safe<T, typename Other::value_type>)
-    constexpr auto to() const {
-        using to_t = typename Other::value_type;
-        if (!util::cast_is_invalid<to_t>(val_)) {
-            return to_unchecked<Other>();
+    constexpr auto as() const {
+        if constexpr (concepts::conversion_safe<T, typename Other::value_type>) {
+            return as_unchecked<Other>();
         } else {
-            throw except::invalid_cast("");
+            using to_t = typename Other::value_type;
+            if (!util::cast_is_invalid<to_t>(val_)) {
+                return as_unchecked<Other>();
+            } else {
+                throw except::invalid_cast("");
+            }
         }
     }
 
     template<concepts::specialisation_of<basic_strict_num> Other>
-    constexpr auto to_unchecked() const {
-        return Other::from_unchecked(std::bit_cast<typename Other::value_type>(val_));
+    constexpr auto as_unchecked() const {
+        return Other::from_unchecked(static_cast<typename Other::value_type>(val_));
     }
 
     template<std::convertible_to<T> V>
-    constexpr static auto from_unchecked(V v) {
+    constexpr static auto from_unchecked(V v) noexcept {
         return basic_strict_num(v, false);
     }
 
-    constexpr basic_strict_num(T v) : val_{v} {}
+    template<std::convertible_to<T> V>
+    explicit constexpr basic_strict_num(V v) : val_{static_cast<T>(v)} {
+        if constexpr (!concepts::conversion_safe_v<V, T>) {
+            if (v != val_) {
+                throw except::invalid_cast("cannot fit " + std::to_string(v) + " into size");
+            }
+        }
+    }
+    constexpr basic_strict_num(const basic_strict_num&) noexcept = default;
+    constexpr basic_strict_num(basic_strict_num&&) noexcept = default;
+    constexpr auto operator=(const basic_strict_num&) noexcept -> basic_strict_num& = default;
+    constexpr auto operator=(basic_strict_num&&) noexcept -> basic_strict_num& = default;
 
-    template<concepts::conversion_safe<T> V>
-    constexpr explicit basic_strict_num(V v) : val_{std::bit_cast<T>(v)} {}
-
-    template<typename OT>
-    constexpr auto operator<=>(basic_strict_num<OT> other) {
+    constexpr auto operator<=>(basic_strict_num other) noexcept {
         return val_ <=> other.val_;
     }
-    template<typename OT>
-    constexpr auto operator==(basic_strict_num<OT> other) {
+    constexpr auto operator==(basic_strict_num other) noexcept {
         return !(*this < other) && !(*this > other);
     }
 
@@ -140,24 +151,23 @@ public:
         return static_cast<V>(val_);
     }
 
-    template<typename RhsV>
-        requires (concepts::same_signidness_v<value_type, RhsV>)
-    constexpr auto operator+(basic_strict_num<RhsV> rhs) {
-        return util::apply_bin<basic_strict_num>(*this, rhs, [](auto lhs, auto rhs) { return lhs + rhs; });
+#define OP_SAME_SIGN(op) \
+    constexpr auto operator op (basic_strict_num rhs) noexcept { \
+        return util::apply_bin<basic_strict_num>(*this, rhs, [](auto lhs, auto rhs) { return lhs op rhs; }); \
     }
+
+    OP_SAME_SIGN(+)
+    OP_SAME_SIGN(-)
+    OP_SAME_SIGN(^)
+    OP_SAME_SIGN(&)
+    OP_SAME_SIGN(*)
+    OP_SAME_SIGN(%)
+    OP_SAME_SIGN(<<)
+    OP_SAME_SIGN(>>)
+#undef OP_SAME_SIGN
+
     template<typename RhsV>
-        requires (concepts::same_signidness_v<value_type, RhsV>)
-    constexpr auto operator-(basic_strict_num<RhsV> rhs) {
-        return util::apply_bin<basic_strict_num>(*this, rhs, [](auto lhs, auto rhs) { return lhs - rhs; });
-    }
-    template<typename RhsV>
-        requires (concepts::same_signidness_v<value_type, RhsV>)
-    constexpr auto operator*(basic_strict_num<RhsV> rhs) {
-        return util::apply_bin<basic_strict_num>(*this, rhs, [](auto lhs, auto rhs) { return lhs * rhs; });
-    }
-    template<typename RhsV>
-        requires (concepts::same_signidness_v<value_type, RhsV> && (std::integral<T> == std::integral<RhsV>))
-    constexpr auto operator/(basic_strict_num<RhsV> rhs) {
+    constexpr auto operator/(basic_strict_num rhs) {
         return util::apply_bin<basic_strict_num>(*this, rhs, [](auto lhs, auto rhs) { return lhs / rhs; });
     }
 
@@ -173,7 +183,7 @@ private:
         return f;
     }
 
-    basic_strict_num(T v, bool) : val_{v} {}
+    constexpr basic_strict_num(T v, bool) noexcept : val_{v} {}
 
     value_type val_;
 
@@ -184,18 +194,28 @@ private:
     requires requires (Lhs l, Rhs r) { \
         { l op r } -> std::convertible_to<Lhs>; \
     } \
-    constexpr auto operator op##= (Lhs& l, Rhs r) -> Lhs& { \
+    constexpr auto operator op##= (Lhs& l, Rhs r) noexcept -> Lhs& { \
         l = l op r; \
         return l; \
     }
 
 IMPL_EQ_OP(<<)
+IMPL_EQ_OP(>>)
+IMPL_EQ_OP(^)
+IMPL_EQ_OP(&)
 IMPL_EQ_OP(*)
 IMPL_EQ_OP(/)
+IMPL_EQ_OP(%)
 IMPL_EQ_OP(-)
 IMPL_EQ_OP(+)
 
+#undef IMPL_EQ_OP
+
 static_assert(concepts::conversion_safe_v<int, long int>);
+
+
+
+
 }
 
 using u8 = detail::basic_strict_num<std::uint8_t>;
@@ -204,6 +224,7 @@ using u32 = detail::basic_strict_num<std::uint32_t>;
 using u64 = detail::basic_strict_num<std::uint64_t>;
 using i32 = detail::basic_strict_num<std::int32_t>;
 using i64 = detail::basic_strict_num<std::int64_t>;
+
 
 }
 
--- test/main.cpp
+++ test/main.cpp
@@ -6,6 +6,8 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
 
+using namespace sn;
+
 namespace sn::detail {
 
 template<sn::concepts::specialisation_of<sn::detail::basic_strict_num> T>
@@ -20,14 +22,11 @@ concept check_is_valid = requires(Func f) {
     std::invoke(f);
 };
 
-#define DO_CHECK(args, expr, msg, name, valid) \
-    template<typename T> \
-    concept name = requires args expr; \
-    static_assert(name <void> == valid, msg)
+#define DO_CHECK(args, expr, msg, name, valid) static_assert((requires args expr) == valid, msg)
 
 
-#define CHECK_VALID(args, expr, msg) DO_CHECK(args, expr, msg, tests_check_valid##__LINE__, true)
-#define CHECK_INVALID(args, expr, msg) DO_CHECK(args, expr, msg, tests_check_invalid##__LINE__, false)
+#define CHECK_VALID(args, expr, msg) DO_CHECK(args, expr, msg, tests_check_valid, true)
+#define CHECK_INVALID(args, expr, msg) DO_CHECK(args, expr, msg, tests_check_invalid, false)
 
 /*
 template<typename T>
@@ -35,9 +34,14 @@ concept cast_test = requires(sn::u16 v) {
     sn::u16(127).to<sn::u8>();
 };*/
 
-CHECK_VALID((), { sn::u16(277).to<sn::u16>(); }, "to cast");
+CHECK_VALID((), { sn::u16(277).as<sn::u32>(); }, "to cast");
 
+template<typename F, typename T>
+concept check_as = requires(F v) {
+    v.template as<T>();
+};
 
+static_assert(check_as<u32, u32>, "can convert to own type safely");
 
 template<typename Lhs, typename Rhs>
 concept adding_test = requires(Lhs lhs, Rhs rhs) {
@@ -53,12 +57,13 @@ static_assert(!adding_test<sn::i32, sn::u32>, "adding different signs is invalid
 static_assert(adding_test<sn::i32, sn::i32>, "adding different signs is valid");
 
 static_assert(adding_test_retr<sn::i32, sn::i32, sn::i32>, "adding same width results in same type");
-static_assert(adding_test_retr<sn::i64, sn::i32, sn::i64>, "adding different widths results in the larger type");
 
 TEST_CASE("adding works") {
     sn::u32 l{32};
     sn::u8 r{7};
-    CHECK(l + r == sn::u32{32 + 7});
+    CHECK(l + r.as<u32>() == sn::u32{32 + 7});
+    l += u32{3};
+    CHECK(l == u32{32 + 3});
 }
 
 TEST_CASE("shifting works") {
@@ -66,4 +71,16 @@ TEST_CASE("shifting works") {
     l <<= 2;
     CHECK(l == sn::u32{65 << 2});
 }
+
+TEST_CASE("numeric conversions") {
+    constexpr auto vnum = 277;
+    u16 v{vnum};
+    SUBCASE("checked as is safe for valid values") {
+        CHECK(v.as<u64>() == u64{vnum});
+    }
+    SUBCASE("checked as throws for narrowing conversions") {
+        CHECK_THROWS(v.as<u8>());
+    }
+}
+constexpr u8 v2 = u16{277}.as<u8>();
 
